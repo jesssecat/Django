6d62a2958a39a895e8d15755c5cf97f4fd569faa
jesssecat <lampjesse@163.com>
2018-11-28 16:41:32 +0800

    ORM

--- orm.py
+++ orm.py
@@ -50,3 +50,44 @@ ret = models.Book.objects.all().count()
 
 #first() 和last()取出第一条和最后一条
 
+#单表查询之神奇的双下划线
+# models.Book.objects.filter(id__lt=10, id__gt=1)  # 获取id大于1 且 小于10的值
+#
+# models.Book.objects.filter(id__in=[11, 22, 33])  # 获取id等于11、22、33的数据
+# models.Book.objects.exclude(id__in=[11, 22, 33])  # not in
+#
+# models.Book.objects.filter(name__contains="ven")  # 获取name字段包含"ven"的
+# models.Book.objects.filter(name__icontains="ven")  # icontains大小写不敏感
+#
+# models.Book.objects.filter(id__range=[1, 3])  # id范围是1到3的，等价于SQL的bettwen and
+#
+# 类似的还有：startswith，istartswith, endswith, iendswith　
+
+
+#外建的查询操作
+#正向查询
+#第一种办法
+book_obj = models.Book.objects.all().first()
+ret = book_obj.chuban #出版是另外一个表，这个表名不用大写
+# print(ret, type(ret))#我是一个出版社：测试出版  #obj
+# ret = book_obj.chuban.name
+# print(ret, type(ret)) #str
+
+#第二种办法
+#利用双下划线进行跨表查询
+#查询id为1的书的出版社名称
+#双下划线跨表查询夸了一个表
+ret = models.Book.objects.filter(id=2).values("chuban__name")
+# print(ret)
+
+
+#反向查询
+#将出版社出版的书查询出来
+chuban_obj = models.Chuban.objects.first()
+ret = chuban_obj.book_set.all()
+# print(ret)
+
+#根据出版社的ID查询出所出版的书籍
+#查询出版社id为1，书的名称
+ret = models.Chuban.objects.filter(id=1).values_list("book__title")
+print(ret)
